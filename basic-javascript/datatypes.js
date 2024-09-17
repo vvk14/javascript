d953d19d6587ed9941e0c5881b204b6e278cc154
Vivek <111179413+vvk14@users.noreply.github.com>
2024-09-17 10:20:04 +0000

    datatypes

---
+++ basic-javascript/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code of newer version
+
+//code readablity must important
+
+console.log(2 + 2)
+console.log("Vivek is learning JS")
+
+let vivek = "Add some value"; // Declare the variable 'vivek'
+console.log(vivek); // This will log the value of 'vivek' If we only print or console vivek then it gives error because vivek is taken as a varaible so we need to assign some value
+
+let name = "vivek" // string type
+let age = 24 // numeber
+let status = false // boolean
+
+// bigint 
+// null => standalone value
+// undefined
+// symbol => unique
+
+//object 
+
+//How to find data types
+console.log(typeof "name")
+console.log(typeof age);
+console.log(typeof status);
+console.log(typeof null); // Type is object
+console.log(typeof undefined); // Type is undefined
